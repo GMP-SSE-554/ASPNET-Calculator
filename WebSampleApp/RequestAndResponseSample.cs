3f7b9a09968fec56fb2e723c2c71c4a28934f1b8
SeditiousSnake <tyson31088@gmail.com>
2018-03-21 19:05:12 -0400

    Add project files.

---
+++ WebSampleApp/RequestAndResponseSample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSampleApp
+{
+    public class RequestAndResponseSample
+    {
+        public static string GetDiv(string key, string value) =>
+            $"<div><span>{key}:</span> <span>{value}</span></div>";
+
+        public static string GetRequestInformation(Microsoft.AspNetCore.Http.HttpRequest request)
+        {
+            var sb = new System.Text.StringBuilder();
+            sb.Append(GetDiv("scheme", request.Scheme));
+            sb.Append(GetDiv("host", request.Host.HasValue ? request.Host.Value : "no host"));
+            sb.Append(GetDiv("path", request.Path));
+            sb.Append(GetDiv("query string", request.QueryString.HasValue ? request.QueryString.Value : "no query string"));
+
+            sb.Append(GetDiv("method", request.Method));
+            sb.Append(GetDiv("protocol", request.Protocol));
+
+            return sb.ToString();
+        }
+    }
+}
